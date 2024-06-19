376536bfca1c8aa0737418a675dde01bb5bd01c5
Pereira Damian <dpereira3@abc.gob.ar>
2024-06-19 11:31:22 -0300

    cambio

--- src/Components/GitHup/Data.jsx
+++ src/Components/GitHup/Data.jsx
@@ -7,7 +7,7 @@ class Data extends Component {
         super(props);
         this.state = { user: [] };
         
-        console.log('username', this.props.params.Id);
+        //console.log('username', this.props.params.Id);
         const FetchUsers = async (user) => {
             const API_Call = await fetch(`https://api.github.com/search/users?q=${user}`);
             const data = await API_Call.json();
@@ -19,6 +19,7 @@ class Data extends Component {
             console.log('the data', res.data.items);
         })
     }
+    
     render() {
        return (
         <React.Fragment>
@@ -26,73 +27,37 @@ class Data extends Component {
                 <div className="album py-5 bg-light">
                 <div className="container">
                     <div className="row">                   
-                    <div className="col-md-3 cp">
-                        <div className="card mb-4 shadow-sm">
-                        <img className="bd-placeholder-img card-img-top" 
+                    
+                    { this.state.user.map( (user) => {
+                        <div className="col-md-3 cp" key={ user.login }>
+                            <div className="card mb-4 shadow-sm">
+                            <img className="bd-placeholder-img card-img-top" 
+                            width="100%" height="225" 
+                            src={ user.avatoar_url } 
+                            alt=''/>
+                            <div className="card-body">
+                                <p className="card-text text-center">
+                                Name : { user.login }
+                                </p>
+                            </div>
+                            </div>
+                        </div>
+                    }) }
+                    {/* <div class="col-md-3 cp">
+                        <div class="card mb-4 shadow-sm">
+                        <img class="bd-placeholder-img card-img-top" 
                         width="100%" height="225" 
                         src="https://avatars0.githubusercontent.com/u/59670?v=4" 
                         alt=''/>
-                        <div className="card-body">
-                            <p className="card-text text-center">
+                        <div class="card-body">
+                            <p class="card-text text-center">
                             Name : Ahmed
                             </p>
                         </div>
                         </div>
-                    </div>
-                    <div className="col-md-3 cp">
-                    <div className="card mb-4 shadow-sm">
-                        <img className="bd-placeholder-img card-img-top"
-                        width="100%" height="225" src="https://avatars0.githubusercontent.com/u/59670?v=4" 
-                        alt=''/>
-                        <div className="card-body">
-                        <p className="card-text text-center">
-                        Name : Ahmed
-                        </p>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="col-md-3 cp">
-                    <div className="card mb-4 shadow-sm">
-                        <img className="bd-placeholder-img card-img-top" 
-                        width="100%" height="225" 
-                        src="https://avatars0.githubusercontent.com/u/59670?v=4" 
-                        alt=''/>
-                        <div className="card-body">
-                        <p className="card-text text-center">
-                        Name : Ahmed
-                        </p>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="col-md-3 cp">
-                    <div className="card mb-4 shadow-sm">
-                        <img 
-                        className="bd-placeholder-img card-img-top"
-                        width="100%" height="225" 
-                        src="https://avatars0.githubusercontent.com/u/59670?v=4" 
-                        alt=''/>
-                        <div className="card-body">
-                        <p className="card-text text-center">
-                        Name : Ahmed
-                        </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-3 cp">
-                    <div className="card mb-4 shadow-sm">
-                        <img 
-                        className="bd-placeholder-img card-img-top" 
-                        width="100%" height="225" 
-                        src="https://avatars0.githubusercontent.com/u/59670?v=4" 
-                        alt=''/>
-                        <div className="card-body">
-                        <p className="card-text text-center">
-                        Name : Ahmed
-                        </p>
-                        </div>
-                    </div>
-                </div>  
-                </div>
+                    </div> */}
+
+                    </div>  
                 </div>
                 </div>
             </main>
